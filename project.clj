601771c895703e2c1d8181bdcf4afdd187488f96
Paul Burt <paul.burt@gmail.com>
2014-06-30 13:27:41 +0100

    Added dependency on lein-bower to handle installation of bower components. Update project.clj files and Makefile to reflect this change.

--- project.clj
+++ project.clj
@@ -3,16 +3,17 @@
   :url "http://github.com/holmsand/reagent"
   :license {:name "MIT"}
   :description "A simple ClojureScript interface to React"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]]
-  :plugins [[lein-cljsbuild "1.0.2"]
-            [com.cemerick/clojurescript.test "0.2.2"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2234"]]
+  :plugins [[lein-bower "0.5.1"]
+            [lein-cljsbuild "1.0.3"]
+            [com.cemerick/clojurescript.test "0.3.1"]]
   :profiles {:prod {:cljsbuild
                     {:builds
                      {:client {:compiler
                                {:optimizations :advanced
                                 :elide-asserts true
-                                :preamble ^:replace ["reagent/react.min.js"]
+                                :preamble ^:replace ["react/react.min.js"]
                                 :pretty-print false}}}}}
              :test {:cljsbuild
                     {:builds
@@ -28,7 +29,8 @@
                          {:source-map "target/cljs-client.js.map"
                           :source-map-path "client"}}}}}}
   :source-paths ["src"]
-  :resource-paths ["vendor"]
+  :bower {:directory "resources"}
+  :bower-dependencies [[react "0.10.0"]]
   :cljsbuild
   {:builds
    {:client {:source-paths ["src" "demo" "examples/todomvc/src"
@@ -36,7 +38,7 @@
                             "examples/geometry/src"]
              :notify-command ["node" "./bin/gen-site.js"]
              :compiler
-             {:preamble ["reagent/react.js"]
+             {:preamble ["react/react.js"]
               :output-dir "target/client"
               :output-to "target/cljs-client.js"
               :pretty-print true}}}})
