76412889821602070c547ed61df77e96ab6c0272
Dan Holmsand <holmsand@gmail.com>
2015-02-07 09:30:20 +0100

    Use figwheel 0.2.3 and cleanup of project.clj

--- project.clj
+++ project.clj
@@ -7,56 +7,47 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2760"]
                  [cljsjs/react "0.12.2-5"]]
+
   :plugins [[lein-cljsbuild "1.0.4"]]
-  :resource-paths ["vendor"]
+
   :source-paths ["src"]
   
-  :profiles {:base {:cljsbuild {:builds
-                                    {:client
-                                     {:source-paths
-                                      ["src"
-                                       "demo"
-                                       "examples/todomvc/src"
-                                       "examples/simple/src"
-                                       "examples/geometry/src"]}}}}
+  :profiles {:test {:cljsbuild
+                    {:builds {:client {:source-paths ["test"]}}}}
 
-             :test {:cljsbuild {:builds
-                                {:client {:source-paths ["test"]}}}}
-
-             :dev-base {:dependencies
-                        [[figwheel "0.2.2-SNAPSHOT"]]
-                        :plugins [[lein-figwheel "0.2.2-SNAPSHOT"]]
+             :dev-base {:dependencies [[figwheel "0.2.3-SNAPSHOT"]]
+                        :plugins [[lein-figwheel "0.2.3-SNAPSHOT"]]
                         :source-paths ["demo"] ;; for lighttable
                         :resource-paths ["site" "outsite"]
                         :figwheel {:css-dirs ["site/public/css"]}
-                        :cljsbuild {:builds
-                                    {:client
-                                     {:source-paths ["env/dev"]
-                                      :compiler {:main "devsetup"
-                                                 :source-map true
-                                                 :source-map-timestamp true
-                                                 :optimizations :none
-                                                 :output-dir
-                                                 "outsite/public/js/out"}}}}}
+                        :cljsbuild
+                        {:builds
+                         {:client
+                          {:source-paths ["env/dev"]
+                           :compiler {:main "devsetup"
+                                      :source-map true
+                                      :source-map-timestamp true
+                                      :optimizations :none
+                                      :output-dir "outsite/public/js/out"
+                                      :asset-path "js/out"}}}}}
              
-             :site {:resource-paths ^:replace ["outsite" "vendor"]
+             :site {:resource-paths ^:replace ["outsite"]
                     :figwheel {:css-dirs ^:replace ["outsite/public/css"]}
-                    :cljsbuild {:builds
-                                {:client
-                                 {:notify-command
-                                  ["node" "bin/gen-site.js"]}}}}
+                    :cljsbuild
+                    {:builds {:client
+                              {:notify-command ["node" "bin/gen-site.js"]}}}}
              
-             :prod [:base :site
-                    {:cljsbuild {:builds
-                                 {:client
-                                  {:source-paths ["env/prod"]
-                                   :compiler {:main "prodsetup"
-                                              :optimizations :advanced
-                                              :elide-asserts true
-                                              :pretty-print false
-                                              :output-dir "target/client"}}}}}]
+             :prod [:site
+                    {:cljsbuild
+                     {:builds {:client
+                               {:source-paths ["env/prod"]
+                                :compiler {:main "prodsetup"
+                                           :optimizations :advanced
+                                           :elide-asserts true
+                                           :pretty-print false
+                                           :output-dir "target/client"}}}}}]
              
-             :dev [:test :base :dev-base]
+             :dev [:test :dev-base]
              :prod-test [:test :prod]}
 
   :clean-targets ^{:protect false} [:target-path :compile-path
@@ -66,11 +57,13 @@
                                     "outsite/public/index.html"
                                     "out"]
 
-  :cljsbuild {:builds
-              {:client {:compiler
-                        {:output-to "outsite/public/js/main.js"
-                         :asset-path "js/out"}}}}
+  :cljsbuild {:builds {:client
+                       {:source-paths ["src"
+                                       "demo"
+                                       "examples/todomvc/src"
+                                       "examples/simple/src"
+                                       "examples/geometry/src"]
+                        :compiler {:output-to "outsite/public/js/main.js"}}}}
   
   :figwheel {:http-server-root "public" ;; assumes "resources"
-             :repl false
-             :server-port 3449})
+             :repl false})
