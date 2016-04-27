9d05459e1a07cf13913be771c144d69b556230c7
Juho Teperi <juho.teperi@iki.fi>
2016-04-27 23:27:21 +0300

    Update Cljsjs React dependency
    The latest Cljsjs React build adds missing nativeEvent property for
    SyntheticEvent object on the extern file.

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145"]
-                 [cljsjs/react-dom "15.0.1-0"]
-                 [cljsjs/react-dom-server "15.0.1-0"]]
+                 [cljsjs/react-dom "15.0.1-1"]
+                 [cljsjs/react-dom-server "15.0.1-1"]]
 
   :plugins [[lein-cljsbuild "1.1.0"]
             [codox "0.9.0"]]
