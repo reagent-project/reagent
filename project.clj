a0635e0b3471501156d2e74c70b95ee3b450cc32
Juho Teperi <juho.teperi@iki.fi>
2017-06-27 19:05:19 +0300

    Really release 0.7.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reagent "0.7.0-SNAPSHOT"
+(defproject reagent "0.7.0"
   :url "http://github.com/reagent-project/reagent"
   :license {:name "MIT"}
   :description "A simple ClojureScript interface to React"
