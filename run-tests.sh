2279ef4c9dbe18cd33c719d71485ee923c753c51
Juho Teperi <juho.teperi@iki.fi>
2017-10-13 15:34:15 +0300

    Print test summary

--- run-tests.sh
+++ run-tests.sh
@@ -4,10 +4,13 @@
 trap "kill 0" SIGINT
 
 reset='\033[0m'
-blue='\033[0;34m'
 red='\033[0;31m'
+green='\033[0;32m'
+blue='\033[0;34m'
 
 EXIT=0
+
+SUMMARY="$blue##\n## SUMMARY\n##$reset\n\n"
 
 for env in test-environments/*; do
     FAIL=0
@@ -23,11 +26,16 @@ for env in test-environments/*; do
     [[ $? != "0" ]] && FAIL=1
     if [[ $FAIL != "0" ]]; then
         echo
-        echo -e "$red!! FAIL $name$reset"
+        echo -e "${red}FAIL $name$reset"
+        SUMMARY="$SUMMARY${red}FAIL $name$reset\n"
         EXIT=1
+    else
+        SUMMARY="$SUMMARY${green}OK   $name$reset\n"
     fi
     echo
     echo
 done
+
+echo -e "$SUMMARY"
 
 exit $EXIT
