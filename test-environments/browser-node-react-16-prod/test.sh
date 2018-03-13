5594e3bdb072c4362f41b0aaec233b3cfe64f7fc
Juho Teperi <juho.teperi@iki.fi>
2018-03-13 21:49:32 +0200

    Fix test script lein profile

--- test-environments/browser-node-react-16-prod/test.sh
+++ test-environments/browser-node-react-16-prod/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
-lein with-profile dev,node-react-16-prod do clean, doo chrome-headless prod-test once
+lein do clean, doo chrome-headless prod-test once
 test -f target/cljsbuild/prod-test/main.js
 ../../node_modules/.bin/gzip-size target/cljsbuild/prod-test/main.js
