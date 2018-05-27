7989a976134b05d26849ab4ae6f8c579291a5f82
Facundo Olano <facundo.olano@gmail.com>
2018-05-26 22:04:54 -0300

    remove tests

--- demo/reagentdemo/dev.cljs
+++ demo/reagentdemo/dev.cljs
@@ -1,9 +1,8 @@
 (ns reagentdemo.dev
   "Initializes the demo app, and runs the tests."
-  (:require [reagentdemo.core :as core]
-            [reagenttest.runtests :as tests]))
+  (:require [reagentdemo.core :as core]))
 
 (defn init! []
-  (core/init! (tests/init!)))
+  (core/init!))
 
 (init!)
