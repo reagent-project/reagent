4ec10ac146293634b56085e7caa3a59e3f12dcb6
Juho Teperi <juho.teperi@iki.fi>
2018-12-31 15:35:49 +0200

    Bump version to 0.9.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reagent "0.8.2-SNAPSHOT"
+(defproject reagent "0.9.0-SNAPSHOT"
   :url "http://github.com/reagent-project/reagent"
   :license {:name "MIT"}
   :description "A simple ClojureScript interface to React"
