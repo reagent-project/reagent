02f895d8cef42da9fa27fbe12206f105a56e59e7
Juho Teperi <juho.teperi@iki.fi>
2018-12-31 13:45:03 +0200

    Update doo for aget fix

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                  [cljsjs/react-dom-server "16.6.0-0"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]
-            [lein-doo "0.1.10"]
+            [lein-doo "0.1.11"]
             [lein-codox "0.10.3"]
             [lein-figwheel "0.5.18"]]
 
@@ -24,7 +24,7 @@
 
   :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.10.439"]
                                   [figwheel "0.5.18"]
-                                  [doo "0.1.10"]
+                                  [doo "0.1.11"]
                                   [cljsjs/prop-types "15.6.2-0"]]
                    :source-paths ["demo" "test" "examples/todomvc/src" "examples/simple/src" "examples/geometry/src"]
                    :resource-paths ["site" "target/cljsbuild/client" "target/cljsbuild/client-npm"]}}
