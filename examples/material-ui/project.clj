bb8bcc414ee8c4ce9a0035526949f767eb056265
Juho Teperi <juho.teperi@iki.fi>
2019-09-12 10:39:07 +0300

    Update material-ui example deps

--- examples/material-ui/project.clj
+++ examples/material-ui/project.clj
@@ -1,15 +1,15 @@
 (defproject material-ui-reagent "0.6.0"
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/clojurescript "1.10.516"]
-                 [reagent "0.8.1"]
-                 [figwheel "0.5.18"]
-                 [cljsjs/react "16.8.6-0"]
-                 [cljsjs/react-dom "16.8.6-0"]
-                 [cljsjs/material-ui "4.1.0-0"]
-                 [cljsjs/material-ui-icons "3.0.1-0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.520"]
+                 [reagent "0.9.0-rc1"]
+                 [figwheel "0.5.19"]
+                 [cljsjs/react "16.9.0-0"]
+                 [cljsjs/react-dom "16.9.0-0"]
+                 [cljsjs/material-ui "4.4.1-0"]
+                 [cljsjs/material-ui-icons "4.4.1-0"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]
-            [lein-figwheel "0.5.18"]]
+            [lein-figwheel "0.5.19"]]
 
   :figwheel {:repl false
              :http-server-root "public"}
