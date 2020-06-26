ce1dbe1be4534ca556fb05c4fede3fd6ac41b7cf
Juho Teperi <juho.teperi@iki.fi>
2020-06-26 09:45:07 +0300

    Test framer-motion

---
+++ examples/framer-motion/src/example/core.cljs
@@ -0,0 +1,28 @@
+(ns example.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]
+            ["framer-motion" :refer [motion]]))
+
+(set! *warn-on-infer* true)
+
+(defn animated []
+  [:> (.-div motion)
+   {:initial {:opacity 0}
+    :animate {:opacity 1}
+    :exit {:opacity 0}}
+   "Animated element"])
+
+(defn main []
+  (let [show (r/atom false)]
+    (fn []
+      [:div
+       (if @show
+         [animated])
+       [:button
+        {:on-click #(swap! show not)}
+        "Toggle"]])))
+
+(defn start []
+  (rdom/render [main] (js/document.getElementById "app")))
+
+(start)
