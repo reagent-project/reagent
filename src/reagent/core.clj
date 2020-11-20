0b0042879595d22b4ca01a7ae8f5bdb84035f48c
Juho Teperi <juho.teperi@iki.fi>
2020-11-20 10:52:55 +0200

    Add reagent.core/render macro, throwing analysis-error with note

--- src/reagent/core.clj
+++ src/reagent/core.clj
@@ -1,5 +1,6 @@
 (ns reagent.core
-  (:require [reagent.ratom :as ra]))
+  (:require [reagent.ratom :as ra]
+            [cljs.analyzer.api :as ana-api]))
 
 (defmacro with-let
   "Bind variables as with let, except that when used in a component
@@ -8,3 +9,16 @@
   destroyed."
   [bindings & body]
   `(ra/with-let ~bindings ~@body))
+
+(defn- source-info [env]
+  (when (:line env)
+    {:file (ana-api/current-file)
+     :line (:line env)
+     :column (:column env)}))
+
+(defmacro render
+  "Render function was moved to reagent.dom namespace in 1.0"
+  [& body]
+  (throw (ex-info "reagent.core/render function was moved to reagent.dom namespace in Reagent v1.0"
+                  (assoc (source-info &env) :tag :cljs/analysis-error)))
+  nil)
