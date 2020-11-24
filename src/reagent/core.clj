59763953b757c9607d36b248d05ba4f2b0adebdd
Juho Teperi <juho.teperi@iki.fi>
2020-11-24 16:19:40 +0200

    Add render fn throwing runtime error about function being moved

--- src/reagent/core.clj
+++ src/reagent/core.clj
@@ -1,6 +1,5 @@
 (ns reagent.core
-  (:require [reagent.ratom :as ra]
-            cljs.analyzer.api))
+  (:require [reagent.ratom :as ra]))
 
 (defmacro with-let
   "Bind variables as with let, except that when used in a component
@@ -9,20 +8,3 @@
   destroyed."
   [bindings & body]
   `(ra/with-let ~bindings ~@body))
-
-(defn- source-info [env]
-  (when (:line env)
-    {:file (try
-             ((resolve 'cljs.analyzer.api/current-file))
-             (catch Exception _
-               ;; ana-api/current-file was added in 1.10.758
-               cljs.analyzer/*cljs-file*))
-     :line (:line env)
-     :column (:column env)}))
-
-(defmacro render
-  "Render function was moved to reagent.dom namespace in 1.0"
-  [& body]
-  (throw (ex-info "reagent.core/render function was moved to reagent.dom namespace in Reagent v1.0"
-                  (assoc (source-info &env) :tag :cljs/analysis-error)))
-  nil)
