2a8ee052faa42170512efc8a32691d635e97abe0
Juho Teperi <juho.teperi@iki.fi>
2021-04-25 16:20:52 +0300

    Try setting git identity for CI pushes

--- build-docs.sh
+++ build-docs.sh
@@ -13,6 +13,8 @@ lein codox
 
 rm -fr tmp
 if [[ -n $GITHUB_ACTOR ]]; then
+    git config --global user.email "14146879+github-actions[bot]@users.noreply.github.com"
+    git config --global user.name "github-actions[bot]"
     git clone "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/reagent-project/reagent-project.github.io.git" tmp
 else
     git clone git@github.com:reagent-project/reagent-project.github.io.git tmp
