c5e5cd256d4a366ae666e9cc9fc5351bef3696e9
Juho Teperi <juho.teperi@iki.fi>
2021-04-25 15:59:18 +0300

    Remove gzip-size-cli

--- test-environments/browser-cljsjs-prod/test.sh
+++ test-environments/browser-cljsjs-prod/test.sh
@@ -3,4 +3,6 @@ set -ex
 rm -rf target/cljsbuild/prod-test/
 lein doo chrome-headless prod-test once
 test -f target/cljsbuild/prod-test/main.js
-npx gzip-size-cli target/cljsbuild/prod-test/main.js
+
+gzip -k target/cljsbuild/prod-test/main.js
+ls -lh target/cljsbuild/prod-test/
