d0858ae8d089ccfeb9c062ac445fa673d5bf733b
Juho Teperi <juho.teperi@iki.fi>
2021-04-25 13:05:24 +0300

    Fix npx calls

--- test-environments/browser-cljsjs-prod/test.sh
+++ test-environments/browser-cljsjs-prod/test.sh
@@ -3,4 +3,4 @@ set -ex
 rm -rf target/cljsbuild/prod-test/
 lein doo chrome-headless prod-test once
 test -f target/cljsbuild/prod-test/main.js
-node_modules/.bin/gzip-size target/cljsbuild/prod-test/main.js
+npx gzip-size-cli target/cljsbuild/prod-test/main.js
--- test-environments/browser-npm-prod/test.sh
+++ test-environments/browser-npm-prod/test.sh
@@ -5,4 +5,4 @@ set -ex
 rm -rf target/cljsbuild/prod-test-npm/
 lein doo chrome-headless prod-test-npm once
 test -f target/cljsbuild/prod-test-npm/main.js
-node_modules/.bin/gzip-size target/cljsbuild/prod-test-npm/main.js
+npx gzip-size-cli target/cljsbuild/prod-test-npm/main.js
