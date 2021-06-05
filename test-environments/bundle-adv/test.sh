3e209b30c3362ae7038dc848e6da6be3e246a139
Juho Teperi <juho.teperi@iki.fi>
2021-06-05 14:19:10 +0300

    Fix clojure cli opts

--- test-environments/bundle-adv/test.sh
+++ test-environments/bundle-adv/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-clojure -m cljs.main -co test-environments/bundle-adv/karma.edn -v --compile
+clojure -M -m cljs.main -co test-environments/bundle-adv/karma.edn -v --compile
 npx karma start test-environments/bundle-adv/karma.conf.js
 
 gzip -fk target/bundle-adv/resources/public/js/out/karma.js
--- test-environments/bundle/test.sh
+++ test-environments/bundle/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
-clojure -m cljs.main -co test-environments/bundle/build.edn --compile
+clojure -M -m cljs.main -co test-environments/bundle/build.edn --compile
 npx karma start test-environments/bundle/karma.conf.js
