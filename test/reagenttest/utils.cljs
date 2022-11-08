1bd842df03ed520b57b2628d527359e8a3af1f67
Juho Teperi <juho.teperi@iki.fi>
2022-11-08 21:57:08 +0200

    Add test act util

--- test/reagenttest/utils.cljs
+++ test/reagenttest/utils.cljs
@@ -1,8 +1,42 @@
 (ns reagenttest.utils
   (:require ["react" :as react]
+            ["react-dom/test-utils" :as react-test]
             [reagent.core :as r]
             [reagent.dom :as rdom]
             [reagent.impl.template :as tmpl]))
+
+;; Seems to be similar to what React is doing:
+;; https://github.com/facebook/react/blob/v18.2.0/packages/react-devtools-shared/src/__tests__/setupTests.js#L93
+(defonce original-console-error (.-error js/console))
+
+(set! (.-error js/console)
+      (fn [& [first-arg :as args]]
+        (cond
+          (.startsWith first-arg "Warning: The current testing environment is not configured to support act")
+          nil
+
+          :else
+          (apply original-console-error args))))
+
+(defn act
+  "Run f to trigger Reagent updates,
+  will return Promise which will resolve after
+  Reagent and React render."
+  [f]
+  ;; async act doesn't return a real promise (with chainable then),
+  ;; so wrap it.
+  (js/Promise.
+    (fn [resolve reject]
+      (.then (react-test/act
+               (fn reagent-act-callback []
+                 (f)
+                 ;; React act callback should return something "thenable" to use
+                 ;; async act.
+                 (js/Promise. (fn [resolve _reject]
+                                (r/after-render (fn reagent-act-after-reagent-flush []
+                                                  (resolve)))))))
+             resolve
+             reject))))
 
 (defn with-mounted-component
   ([comp f]
