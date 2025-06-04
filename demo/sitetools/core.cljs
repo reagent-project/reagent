b0d12d19ce91cfe789db749d8570b42b1eae6a25
Juho Teperi <juho.teperi@iki.fi>
2025-06-04 16:39:48 +0300

    Disable StrictMode on demo site
    StrictMode breaks render fn side-effects, like the timer component on
    the demo site, because React will call render twice to ensure no
    side-effects.

--- demo/sitetools/core.cljs
+++ demo/sitetools/core.cljs
@@ -120,4 +120,5 @@
       ;; Enable StrictMode to warn about e.g. findDOMNode
       ;; NOTE: timer-component will go forward 2 seconds at a time, because the
       ;; render fn will be called twice and it will register two timeouts.
-      (rdomc/render @react-root [:> react/StrictMode {} body]))))
+      ; (rdomc/render @react-root [:> react/StrictMode {} body])
+      (rdomc/render @react-root body))))
