e7f1a2a6885e35c711320c0eb5ebcc6f8d3b0ab7
Juho Teperi <juho.teperi@iki.fi>
2025-09-26 16:19:07 +0300

    Add clj value handling to hooks

--- test/reagenttest/testhooks.cljs
+++ test/reagenttest/testhooks.cljs
@@ -14,13 +14,13 @@
 (def ran (atom 0))
 (def update-state (atom nil))
 
-(r/defc foo1 []
+(r/defc state-1 []
   (let [[x set-x] (hooks/use-state 0)]
     (reset! update-state set-x)
     (swap! ran inc)
     [:div "foo " x]))
 
-(r/defc foo2 []
+(r/defc state-2 []
   (let [[x set-x] (hooks/use-state {:foo 0})]
     (reset! update-state set-x)
     (swap! ran inc)
@@ -29,7 +29,8 @@
 (deftest ^:dom use-state-test
   (u/async
     (testing "update state, pure value"
-      (u/with-render [div [foo1]]
+      (reset! ran 0)
+      (u/with-render [div [state-1]]
         (is (= 1 @ran))
         (is (= "foo 0" (.-innerText div)))
 
@@ -39,7 +40,7 @@
 
     (testing "update state, clojure value"
       (reset! ran 0)
-      (u/with-render [div [foo2]]
+      (u/with-render [div [state-2]]
         (is (= 1 @ran))
         (is (= "foo 0" (.-innerText div)))
 
@@ -53,3 +54,39 @@
         (is (= 2 @ran))
         (is (= "foo 1" (.-innerText div)))))))
 
+(def effect-ran (atom 0))
+
+(r/defc effect-1 [x]
+  (hooks/use-effect (fn []
+                      (swap! effect-ran inc)
+                      nil)
+                    [x])
+  (swap! ran inc)
+  [:div "foo " (:foo x)])
+
+(deftest ^:dom use-effect-test
+  (u/async
+    (testing "update state, pure value"
+      (reset! ran 0)
+      (reset! effect-ran 0)
+      (let [x (r/atom {:foo 0})
+            y (r/atom 0)
+            c (fn []
+                [effect-1 @x @y])]
+        (u/with-render [div [c]]
+          (is (= 1 @ran))
+          (is (= 1 @effect-ran))
+          (is (= "foo 0" (.-innerText div)))
+
+          (u/act (swap! x assoc :foo 1))
+          (is (= 2 @ran))
+          (is (= 2 @effect-ran))
+          (is (= "foo 1" (.-innerText div)))
+
+          ;; use-effect wrapper considers clojure equality
+          (u/act (swap! x assoc :foo 1)
+                 (swap! y inc))
+          (is (= 3 @ran))
+          ;; effect didn't ran again because dependency value is the same
+          (is (= 2 @effect-ran))
+          (is (= "foo 1" (.-innerText div))))))))
